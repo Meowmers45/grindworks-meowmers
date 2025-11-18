b3d753881d328033140ae0261a38844b16bb5d00
Aaron Johnson <dynomitekid101+1@gmail.com>
2025-11-17 22:01:07 -0600

    Update item_black_athletic_shoes.gd

--- objects/items/custom/item_black_athletic_shoes.gd
+++ objects/items/custom/item_black_athletic_shoes.gd
@@ -10,11 +10,13 @@ func on_load(_item: Item) -> void:
 func setup() -> void:
 	BattleService.s_battle_started.connect(on_battle_started)
 
-func on_battle_started() -> void:
+func on_battle_started(manager: BattleManager) -> void:
 	await get_tree().process_frame
 	if Util.get_player().character.character_name == "Meowmers":
 		Util.get_player().use_accuracy = 1
+		manager.battle_ui.refresh_tracks()
 	elif Util.get_player().character.character_name == "Aaron":
 		Util.get_player().use_accuracy = 1
+		manager.battle_ui.refresh_tracks()
 	else:
 		return
