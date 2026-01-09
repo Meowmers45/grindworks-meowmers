10e839b103c0195ddb2c94ef6fa65c781545cf48
Aaron Johnson <dynomitekid101+1@gmail.com>
2026-01-09 17:08:21 -0600

    Update item_meowmers_setup.gd

--- objects/items/custom/item_meowmers_setup.gd
+++ objects/items/custom/item_meowmers_setup.gd
@@ -1,7 +1,5 @@
 extends ItemCharSetup
 
-const MEOWMERS_PRANK_PATH := "res://objects/items/resources/active/doodle_chest.tres"
-
 func first_time_setup(player : Player) -> void:
 	var stats := player.stats
 	player.stats.gags_unlocked['Sound'] = 2
@@ -9,7 +7,7 @@ func first_time_setup(player : Player) -> void:
 	player.stats.gag_effectiveness['Squirt'] = 1.0
 	player.stats.funny_dance = true
 	player.stats.pink_slips = 5
-	player.stats.healing_effectiveness = 2.4
+	player.stats.healing_effectiveness = 3.0
 	player.stats.gag_cap = 20
 	player.stats.debug_gag_points = false
 	player.stats.meowmersdragonwings = true
