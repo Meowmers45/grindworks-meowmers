0b70232f936596728712594da6bf19707a059379
Aaron Johnson <dynomitekid101+1@gmail.com>
2026-03-02 15:33:24 -0600

    got rid of the mods unpacked folder (it will be readded later)

--- mods-unpacked/3ngrindworksmods-Difficulty_Enhancer_Plus_More_Proxies/extensions/objects/battle/battle_node/dynamic/battle_node_dynamic.hooks.gd
+++
@@ -1,7 +0,0 @@
-extends Object
-
-var SettingsConfig = ModLoaderConfig.get_config("3ngrindworksmods-Difficulty_Enhancer_Plus_More_Proxies", "settings")
-
-func spawn_cogs(chain: ModLoaderHookChain,cog_count := 1) -> void:
-	var minimum_cogs = max(SettingsConfig.data["Minimum cog amount"], cog_count) 
-	chain.execute_next([minimum_cogs])
